5f7581222da643f10e95c8715290419a8e37baa1
sdnr-tksz <sedanurtoksoz1@gmail.com>
2025-07-25 20:32:15 +0300

    Entity Layer is completed.

---
+++ EntityLayer/Concrete/Newsletter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Newsletter
+    {
+        public int NewsletterId { get; set; }
+        public string Mail { get; set; }
+        
+    
+    }
+}
---
+++ EntityLayer/Concrete/Testimonial.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Testimonial
+    {
+        public int TestimonialId { get; set; }
+        public string Name { get; set; }
+        public string Comment { get; set; }
+        public string Image { get; set; }       
+    }
+}
