e51f3e68b9dda1dee5a8fc742fbd55b13d610de0
sdnr-tksz <sedanurtoksoz1@gmail.com>
2025-07-25 21:57:27 +0300

    The dependencies between layers have been completed and the database connection has been established.

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,26 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=LAPTOP-2F6INANL\\SQLEXPRESS; database=AppleAppDb;integrated security= true;");
+        }
+
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Guide> Guides { get; set; }
+        public DbSet<Newsletter> Newsletters { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+       
+    }
+}
--- EntityLayer/Concrete/Service.cs
+++ EntityLayer/Concrete/Service.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace EntityLayer.Concrete
 {
     public class Service
     {
+        [Key]
         public int ServiceId { get; set; }
         public string? Name { get; set; }
         public string? Description { get; set; }
