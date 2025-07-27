0e883c225b51220aac8a53a79aea90f21b12279c
sdnr-tksz <sedanurtoksoz1@gmail.com>
2025-07-27 19:37:00 +0300

    _UILayout and Home Page have been created. The theme has been added to the wwwroot directory.

--- AppleAppServices/Controllers/LoginServiceController.cs
+++ AppleAppServices/Controllers/LoginServiceController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AppleAppServices.Model;
+using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AppleAppServices.Controllers
@@ -7,5 +9,23 @@ namespace AppleAppServices.Controllers
     [ApiController]
     public class LoginServiceController : ControllerBase
     {
+        [HttpPost]
+        [Route("Login")]
+        public IActionResult Login(LoginModel loginModel)
+        {
+            using (var context = new Context())
+            {
+                var user = context.Guides.SingleOrDefault(u => u.UserName == loginModel.UserName && u.Password == loginModel.Password);
+
+                if (user != null)
+                {
+                    return Ok("Login successful.");
+                }
+                else
+                {
+                    return Unauthorized("Invalid username or password.");
+                }
+            }
+        }
     }
 }
