c83f901866b4062c7bc45aeb8c9ddd02ad3b58f3
sdnr-tksz <sedanurtoksoz1@gmail.com>
2025-07-27 15:32:09 +0300

    In the Concrete folder of the Data Access Layer, the context class has been created to establish the database connection.
    In the Abstract folder of the Data Access Layer, the interfaces have been defined.
    
    In the EntityFramework folder of the Data Access Layer, Entity Framework classes inheriting from the generic repository class have been created.

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,26 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=LAPTOP-2F6INANL\\SQLEXPRESS; database=AppleAppDb;Trusted_Connection=True;TrustServerCertificate=True;Encrypt=False;");
+        }
+
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Guide> Guides { get; set; }
+        public DbSet<Newsletter> Newsletters { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+
+    }
+}
