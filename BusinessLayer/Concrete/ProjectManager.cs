017c89079b3ba035912cd47ef08bacd6fbacce69
sdnr-tksz <sedanurtoksoz1@gmail.com>
2025-07-27 15:45:08 +0300

    In the Abstract folder of the Business Layer, the relevant interfaces have been defined.
    In the Concrete folder of the Business Layer, the corresponding classes have been implemented.

---
+++ BusinessLayer/Concrete/ProjectManager.cs
@@ -0,0 +1,44 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class ProjectManager : IProjectServices
+    {
+        IProjectDal _serviceDal;
+        public ProjectManager(IProjectDal serviceDal)
+        {
+            _serviceDal = serviceDal;
+        }
+        public void TAdd(Project t)
+        {
+            _serviceDal.Insert(t);
+        }
+
+        public void TDelete(Project t)
+        {
+            _serviceDal.Delete(t);
+        }
+
+        public Project TGetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Project> TGetList()
+        {
+            return _serviceDal.GetList();
+        }
+
+        public void TUpdate(Project t)
+        {
+            _serviceDal.Update(t);
+        }
+    }
+}
