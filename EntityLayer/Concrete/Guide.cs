17cb1e2a3be8f3ae45b04b39718a1d14c5fca447
sdnr-tksz <sedanurtoksoz1@gmail.com>
2025-07-27 14:53:44 +0300

    Entities have been created.

---
+++ EntityLayer/Concrete/Guide.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Guide
+    {
+        [Key] public int GuideId { get; set; }
+        public string? Name { get; set; }
+        public string? Department { get; set; }
+        public string? Image { get; set; }
+        public string? LinkedinUrl { get; set; }
+        public string? InstagramUrl { get; set; }
+        public string? XUrl { get; set; }
+
+    }
+}
